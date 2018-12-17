20fffaa8a2250c4b5c05b6281b505f5985986082
uwizeyeh <uwizeyimanahulde1@gmail.com>
2018-12-17 12:23:54 +0200

    add if condition

--- scripts.js
+++ scripts.js
@@ -3,4 +3,8 @@ function getResult(){
     var side1= parseFloat(document.getElementById('side1').value);
     var side2 = parseFloat(document.getElementById('side2').value);
     var side3 = parseFloat(document.getElementById('side3').value);
+
+    if(side1 === side2 && side2 === side3){
+        alert("Equilateral");
+        window.location.href="eqvrt.html";
 }
