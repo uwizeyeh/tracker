2da0e0b3f2447cbe2b2dab9c3d67e63c6ce8ae87
uwizeyeh <uwizeyimanahulde1@gmail.com>
2018-12-17 12:26:18 +0200

    add else  if condition

--- scripts.js
+++ scripts.js
@@ -8,3 +8,15 @@ function getResult(){
         alert("Equilateral");
         window.location.href="eqvrt.html";
 }
+else if(side1 === side2 || side1 === side3 || side2 === side3){
+alert("Isosceles");
+window.location.href="iscol.html";
+}
+else if((side1 + side2) <= side3 || (side1 + side3) <= side2 || (side2 + side3) <= side1) {
+alert("Not a definite triangle");
+}
+else{
+alert("Scalene");
+window.location.href="scal.html";
+}
+}
