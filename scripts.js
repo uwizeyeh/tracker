0d10ef7da3d7a9f1a15c561092d9a3bdc693e861
uwizeyeh <uwizeyimanahulde1@gmail.com>
2018-12-17 12:21:29 +0200

    add array in scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,6 @@
-function getResult()
+var sideofTriangle=["side1","side2","side3"];
+function getResult(){
+    var side1= parseFloat(document.getElementById('side1').value);
+    var side2 = parseFloat(document.getElementById('side2').value);
+    var side3 = parseFloat(document.getElementById('side3').value);
+}
